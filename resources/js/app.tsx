1a92a4914c1255abc10ad6e5e5310be71cc2d379
Robin He <bursteri@gmail.com>
2026-08-01 15:05:32 +0300

    chore: update dependencies and improve type checking
    - Updated various dependencies in package.json for better performance and compatibility.
    - Added a typecheck script to ensure TypeScript types are correct.
    - Refactored Welcome.tsx to remove unnecessary TypeScript error suppression.
    - Enhanced app.tsx and ssr.tsx to utilize the new page component resolution method.
    - Removed baseUrl from tsconfig.json to streamline path resolution.

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,15 +1,16 @@
 import '../css/app.css';
 import './bootstrap';
 
-import { createInertiaApp } from '@inertiajs/react';
+import { createInertiaApp, type ResolvedComponent } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const pages = import.meta.glob<{ default: ResolvedComponent }>('./Pages/**/*.tsx');
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, pages).then((page) => page.default),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -1,18 +1,18 @@
-import { createInertiaApp } from '@inertiajs/react';
+import { createInertiaApp, type ResolvedComponent } from '@inertiajs/react';
 import createServer from '@inertiajs/react/server';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import ReactDOMServer from 'react-dom/server';
 import { RouteName, route } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const pages = import.meta.glob<{ default: ResolvedComponent }>('./Pages/**/*.tsx');
 
 createServer((page) =>
     createInertiaApp({
         page,
         render: ReactDOMServer.renderToString,
         title: (title) => `${title} - ${appName}`,
-        resolve: (name) =>
-            resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')) as any,
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, pages).then((page) => page.default),
         setup: ({ App, props }) => {
             (globalThis as any).route = (name: RouteName, params: any, absolute: boolean) =>
                 route(name, params, absolute, {
